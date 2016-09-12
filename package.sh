d3b1c41743fac71f1d0aa0a1258009a84cc69568
Kris De Volder <kdevolder@pivotal.io>
2016-09-12 15:38:18 -0700

    Create shell script for packaging. Spruce up package.json a bit

---
+++ package.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## run this script to package up this extension using vsce tool.
+## This assumes you have vsce tool installed.
+## You can install it via npm
+
+set -e # fail at the first sign of trouble
+
+mvn clean package ## build language server fat jar
+vsce package
+
+
